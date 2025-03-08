bc3dca3163f52314b3dff0f2a5fba18c9294b1da
David Salas <davidsalas3099@gmail.com>
2025-03-08 13:32:50 -0500

    feat: implements accoutant module and improve theme muk.

---
+++ addons/muk_web_theme/static/src/webclient/navbar/navbar.js
@@ -0,0 +1,57 @@
+import { patch } from '@web/core/utils/patch';
+import { useService } from '@web/core/utils/hooks';
+
+import { NavBar } from '@web/webclient/navbar/navbar';
+import { AppsMenu } from "@muk_web_theme/webclient/appsmenu/appsmenu";
+import { useEffect, useRef } from "@odoo/owl";
+
+patch(NavBar.prototype, {
+    setup() {
+        super.setup();
+        this.appMenuService = useService('app_menu');
+        this.menuButtonRef = useRef('menu_button');
+        this.menuIconRef = useRef('menu_icon');
+        this.dropdownRef = useRef('dropdown_items');
+        this.typeArrowIcon = "oi-arrow-left";
+        useEffect(() => this._updateMenuAppsIcon(), () => []);
+        this.action = this.action.bind(this);
+    },
+
+    _updateMenuAppsIcon() {
+        if (this.menuButtonRef && this.menuButtonRef.el && this.menuIconRef && this.menuIconRef.el) {
+            this.menuButtonRef.el.addEventListener("mouseenter", () => {
+                setTimeout(() => {
+                    this.menuIconRef.el.classList.remove("oi-apps");
+                    this.menuIconRef.el.classList.add(this.typeArrowIcon);
+                }, 300);
+            });
+
+            this.menuButtonRef.el.addEventListener("mouseleave", () => {
+                this.menuIconRef.el.classList.remove("oi-arrow-right"); // Quita la clase nueva
+                this.menuIconRef.el.classList.remove("oi-arrow-left"); // Quita la clase nueva
+                this.menuIconRef.el.classList.add("oi-apps"); // Vuelve a la clase original
+            });
+
+            this.menuButtonRef.el.addEventListener("click", () => {
+                if (this.typeArrowIcon === "oi-arrow-left") {
+                    this.typeArrowIcon = "oi-arrow-right";
+                } else {
+                    this.typeArrowIcon = "oi-arrow-left";
+                }
+            });
+
+        }
+    },
+
+    action(app) {
+        app.action();
+        this.typeArrowIcon = "oi-arrow-left";
+    }
+});
+
+patch(NavBar, {
+    components: {
+        ...NavBar.components,
+        AppsMenu,
+    },
+});
