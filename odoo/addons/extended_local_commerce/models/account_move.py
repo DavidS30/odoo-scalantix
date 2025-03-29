19f976cb3283cb82754c6feab3732215b2a05469
David Salas <davidsalas3099@gmail.com>
2025-03-29 01:02:12 -0500

    fix: ajuste en redondeo de iva en facturas.

--- odoo/addons/extended_local_commerce/models/account_move.py
+++ odoo/addons/extended_local_commerce/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, api
 
 
 class AccountMove(models.Model):
@@ -13,11 +13,19 @@ class AccountMove(models.Model):
             for vals in vals_list:
                 if 'invoice_line_ids' in vals:
                     for line in vals['invoice_line_ids']:
-                        line_data = line[2];
-                        tax_data = line[2]['tax_ids']
+                        line_data = line[2]
+                        tax_data = line_data.get('tax_ids')
                         if tax_data and not tax_data[0][2]:
-                            total_tax_adjust = 1 + iva_tax.amount/100;
-                            line_data['price_unit'] = line_data['price_unit'] / total_tax_adjust # se ajusta precio inicial para todo con iva
-                            line[2]['tax_ids'] = [(6, 0, [iva_tax.id])]  # Se le asigna el IVA
+                            total_tax_adjust = 1 + iva_tax.amount / 100
+                            # Ajustar el precio unitario con redondeo
+                            adjusted_price = round(line_data['price_unit'] / total_tax_adjust, 2)
+                            # Recalcular el precio total con el IVA para verificar
+                            recalculated_total = round(adjusted_price * total_tax_adjust, 2)
+                            # Corregir cualquier diferencia para que coincida con el precio original
+                            difference = line_data['price_unit'] - recalculated_total
+                            if abs(difference) > 0.01:  # Si hay una diferencia significativa
+                                adjusted_price += difference / total_tax_adjust
+                            line_data['price_unit'] = round(adjusted_price, 2)
+                            line_data['tax_ids'] = [(6, 0, [iva_tax.id])]  # Se le asigna el IVA
 
         return super().create(vals_list)
