76671859b337146dc1335986767ecb70d464a257
David Salas <davidsalas3099@gmail.com>
2025-07-03 08:38:50 -0500

    fix: cash issue in report session pos.

--- odoo/addons/extended_local_commerce/models/report_sale_details.py
+++ odoo/addons/extended_local_commerce/models/report_sale_details.py
@@ -7,7 +7,7 @@ class SaleReportInherited(models.AbstractModel):
     def get_sale_details(self, date_start=False, date_stop=False, config_ids=False, session_ids=False, **kwargs):
         # total neto without cashbox
         sale_details = super().get_sale_details(date_start, date_stop, config_ids, session_ids, **kwargs)
-        payment_cash = [payment for payment in sale_details['payments'] if payment['name'].startswith('Efectivo') and payment['cash']]
+        payment_cash = [payment for payment in sale_details['payments'] if payment['name'].startswith('Efectivo') and payment.get('cash', False)]
         payment_cash = payment_cash[0] if payment_cash else None
         total_countent_cash = payment_cash['final_count'] if payment_cash else 0
         total_amount_cash_box = [move for move in payment_cash['cash_moves'] if move['name'] == 'Apertura de caja']
